40309034c006f0e50295e367c008de117aec5f2d
Avinash Pandey <avinash-chandra.pandey@amd.com>
2021-11-26 16:46:59 +0530

    [ZENDNN INFRA] Added env support for ZENDNN_GEMM_ALGO
    1. Added env support for ZENDNN_GEMM_ALGO and
       removed ZENDNN_RELU_UPPERBOUND
    
    Signed-off-by: Avinash Pandey <avinash-chandra.pandey@amd.com>
    Change-Id: I9e8eef667ec06e4ff78b959646660d1d4657d0f5
    Reviewed-on: http://gerrit-git.amd.com/c/amd/ec/ZenDNN/+/606276
    Tested-by: Llvm Cpu <llvm.cpu@amd.com>
    Reviewed-by: Aakar Dwivedi <Aakar.Dwivedi@amd.com>

--- scripts/zendnn_TF_env_setup.sh
+++ scripts/zendnn_TF_env_setup.sh
@@ -205,9 +205,9 @@ echo "ZENDNN_NHWC_BLOCKED=$ZENDNN_NHWC_BLOCKED"
 export ZENDNN_INT8_SUPPORT=0
 echo "ZENDNN_INT8_SUPPORT=$ZENDNN_INT8_SUPPORT"
 
-# INT8 Relu6 fusion support is disabled by default
-export ZENDNN_RELU_UPPERBOUND=0
-echo "ZENDNN_RELU_UPPERBOUND=$ZENDNN_RELU_UPPERBOUND"
+# ZENDNN_GEMM_ALGO is set to 0 by default
+export ZENDNN_GEMM_ALGO=0
+echo "ZENDNN_GEMM_ALGO=$ZENDNN_GEMM_ALGO"
 
 # Switch to enable Conv, Add fusion on users discretion. Currently it is
 # safe to enable this switch for resnet50v1_5, resnet101, and
