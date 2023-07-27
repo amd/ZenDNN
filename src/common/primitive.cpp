6736db2fc6d37c7db94a643b16b0d15ada5a2096
Ishita Shreya <ishita.shreya@amd.com>
2023-07-27 09:38:03 +0000

    [ZENDNN INFRA] Added Profiling Support for ZenDNN.
    1. Linux PERF API Support
    - To adjust the available functionality of perf_events for non-root users
      - sudo sh -c 'echo -2 > /proc/sys/kernel/perf_event_paranoid'
    - ENV variables to enable PERF APIs
      - export ZENDNN_PRIMITIVE_LOG_ENABLE=2
      - export ZENDNN_LOG_OPTS=ALL:-1,PROF:0,PERF:2
    
    2. Uprof Integration to ZenDNN
    - To build ZenDNN library with Uprof Support
      - export UPROF_INSTALL_PATH=/home/<user-id>/AMDuProf_Linux_x64_4.0.341
      - export ZENDNN_ENABLE_UPROF=1
      - export PATH=$PATH:$UPROF_INSTALL_PATH/bin
      - export LD_LIBRARY_PATH=$UPROF_INSTALL_PATH/bin:$LD_LIBRARY_PATH
    - ENV variables to enable Uprof APIs
      - export ZENDNN_PRIMITIVE_LOG_ENABLE=3
      - export ZENDNN_LOG_OPTS=ALL:-1,PROF:0,PERF:0
    - Run the following command for profiling
      - Example : AMDuProfCLI collect --config assess --start-paused -o /home/ishita/UprofOutput ./zendnn_matmul_gelu_test
      - To generate report : AMDuProfCLI report -i /home/ishita/UprofOutput/AMDuProf-zendnn_matmul_gelu_test-EBP_Aug-16-2023_03-55-21
    3. Embedding bag benchmarking setup.
    
    Signed-off-by: Ishita Shreya <ishita.shreya@amd.com>
    Change-Id: Ib59a7aceaaf6246f891452bf42b77add1af1cfee
    Reviewed-on: https://gerrit-git.amd.com/c/amd/ec/ZenDNN/+/898325
    Tested-by: Llvm Cpu <llvm.cpu@amd.com>
    Reviewed-by: Avinash-Chandra Pandey <Avinash-Chandra.Pandey@amd.com>

--- src/common/primitive.cpp
+++ src/common/primitive.cpp
@@ -27,7 +27,7 @@
 #include "engine.hpp"
 
 #if defined(ZENDNN_ENABLE_ITT_TASKS)
-#include "ittnotify.hpp"
+    #include "ittnotify.hpp"
 #endif
 
 #include "primitive.hpp"
@@ -39,7 +39,12 @@
 #include "stream.hpp"
 #include "utils.hpp"
 #include "zendnn_logging.hpp"
+#include "zendnn_perf.hpp"
 #include "common/zendnn_private.hpp"
+
+#if UPROF_ENABLE
+    #include <AMDProfileController.h>
+#endif
 
 using namespace zendnn;
 using namespace zendnn::impl;
@@ -53,7 +58,9 @@ namespace {
 // A proper approach would be an implementation-specific unpoisoning.
 void unpoison_outputs(const exec_args_t &args) {
     for (const auto &arg : args) {
-        if (arg.second.is_const) continue;
+        if (arg.second.is_const) {
+            continue;
+        }
         auto *mem = arg.second.mem;
         void *p;
         mem->get_data_handle(&p);
@@ -71,12 +78,12 @@ nested_scratchpad_t::nested_scratchpad_t(const exec_ctx_t &master_ctx, int key,
     auto scratchpad = master_ctx.get_scratchpad_grantor();
     scratchpad_mem_storage_ = scratchpad.get_memory_storage(key);
     grantor_ = utils::make_unique<memory_tracking::grantor_t>(
-            nested_p->pd()->scratchpad_registry().grantor(
-                    scratchpad_mem_storage_.get(), master_ctx));
+                   nested_p->pd()->scratchpad_registry().grantor(
+                       scratchpad_mem_storage_.get(), master_ctx));
 #ifdef ZENDNN_ENABLE_MEM_DEBUG
     if (scratchpad_debug::is_protect_scratchpad()) {
         scratchpad_debug::protect_scratchpad_buffer(
-                grantor_->get_base_storage(), grantor_->get_registry());
+            grantor_->get_base_storage(), grantor_->get_registry());
     }
 #endif
 }
@@ -85,7 +92,7 @@ nested_scratchpad_t::nested_scratchpad_t(const exec_ctx_t &master_ctx, int key,
 nested_scratchpad_t::~nested_scratchpad_t() {
     if (scratchpad_debug::is_protect_scratchpad()) {
         scratchpad_debug::unprotect_scratchpad_buffer(
-                grantor_->get_base_storage(), grantor_->get_registry());
+            grantor_->get_base_storage(), grantor_->get_registry());
     }
 }
 #else
@@ -93,8 +100,8 @@ nested_scratchpad_t::~nested_scratchpad_t() = default;
 #endif
 
 status_t primitive_create(primitive_iface_t **primitive_iface,
-        const primitive_desc_iface_t *primitive_desc_iface,
-        const cache_blob_t &cache_blob = cache_blob_t()) {
+                          const primitive_desc_iface_t *primitive_desc_iface,
+                          const cache_blob_t &cache_blob = cache_blob_t()) {
 
     std::pair<primitive_iface_t *, bool> p_iface;
 
@@ -102,28 +109,33 @@ status_t primitive_create(primitive_iface_t **primitive_iface,
     // leads to penaly for latency measurements. Environment variable
     // ZENDNN_PRIMITIVE_LOG_ENABLE can be used to optionally enable below
     // primitive log.
-    if(zendnn_getenv_int("ZENDNN_PRIMITIVE_LOG_ENABLE") == 1) {
+    if (zendnn_getenv_int("ZENDNN_PRIMITIVE_LOG_ENABLE") == 1) {
         double start_ms = get_msec();
         CHECK(primitive_desc_iface->create_primitive_iface(
-                p_iface, cache_blob));
+                  p_iface, cache_blob));
         double duration_ms = get_msec() - start_ms;
 
         const char *str = p_iface.second ? "cache_hit," : "cache_miss,";
-        if (cache_blob) str = "from_cache_blob";
+        if (cache_blob) {
+            str = "from_cache_blob";
+        }
 
         std::string stamp;
-        if (get_verbose_timestamp()) stamp = "," + std::to_string(start_ms);
+        if (get_verbose_timestamp()) {
+            stamp = "," + std::to_string(start_ms);
+        }
         zendnnInfo(ZENDNN_PROFLOG, "zendnn_primitive_create,", stamp.c_str(),
-                str, p_iface.first->pd()->info(), ",", duration_ms,  ",ms");
-    } else {
+                   str, p_iface.first->pd()->info(), ",", duration_ms,  ",ms");
+    }
+    else {
         CHECK(primitive_desc_iface->create_primitive_iface(
-                p_iface, cache_blob));
+                  p_iface, cache_blob));
     }
     return safe_ptr_assign((*primitive_iface), p_iface.first);
 }
 
 status_t primitive_execute(
-        const primitive_iface_t *primitive_iface, exec_ctx_t &ctx) {
+    const primitive_iface_t *primitive_iface, exec_ctx_t &ctx) {
     auto stream = ctx.stream();
     status_t status = success;
 
@@ -131,36 +143,94 @@ status_t primitive_execute(
 
 #if defined(ZENDNN_ENABLE_ITT_TASKS)
     const bool enable_itt = itt::get_itt(itt::__itt_task_level_low);
-    if (enable_itt)
+    if (enable_itt) {
         itt::primitive_task_start(primitive_iface->pd()->impl()->kind());
+    }
 #endif
 
     // Default enabling of log and corresponding duration_ms calculation
     // amd stream wait leads to penaly for latency measurements. Environment
     // variable ZENDNN_PRIMITIVE_LOG_ENABLE can be used to optionally enable
     // below primitive execute log.
-    if(zendnn_getenv_int("ZENDNN_PRIMITIVE_LOG_ENABLE") == 1) {
+    if (zendnn_getenv_int("ZENDNN_PRIMITIVE_LOG_ENABLE") == 1) {
         stream->wait();
         double start_ms = get_msec();
         status = stream->enqueue_primitive(primitive_iface, ctx);
         stream->wait();
         double duration_ms = get_msec() - start_ms;
         std::string stamp;
-        if (get_verbose_timestamp()) stamp = "," + std::to_string(start_ms);
+        if (get_verbose_timestamp()) {
+            stamp = "," + std::to_string(start_ms);
+        }
 
         zendnnInfo(ZENDNN_PROFLOG, "zendnn_primitive_execute,", stamp.c_str(),
-            primitive_iface->pd()->info(), ",", duration_ms, ",ms");
-    } else {
+                   primitive_iface->pd()->info(), ",", duration_ms, ",ms");
+    }
+
+    else if (zendnn_getenv_int("ZENDNN_PRIMITIVE_LOG_ENABLE") == 2) {
+        stream->wait();
+
+        //PERF PROFILE
+        single_event ipc;
+        single_event llc;
+        ipc.open_event(event_type::IPC);
+        llc.open_event(event_type::LLC_MISS_RATE);
+        ipc.start_event("zendnn_primitive_execute");
+        llc.start_event("zendnn_primitive_execute");
+
+        double start_ms = get_msec();
+        status = stream->enqueue_primitive(primitive_iface, ctx);
+        stream->wait();
+        double duration_ms = get_msec() - start_ms;
+
+        //PERF PROFILE
+        ipc.stop_event();
+        llc.stop_event();
+        double ipc_val=ipc.read_event();
+        double llc_val=llc.read_event();
+        ipc.close_event();
+        llc.close_event();
+
+        std::string stamp;
+        if (get_verbose_timestamp()) {
+            stamp = "," + std::to_string(start_ms);
+        }
+        //PERF PROFILE
+        zendnnInfo(ZENDNN_PERFLOG, "zendnn_primitive_execute,",
+                   primitive_iface->pd()->info(),",",duration_ms, ",ms",",",ipc.event_in,":",
+                   ipc_val," , ",llc.event_in,":",llc_val);
+    }
+    else if (zendnn_getenv_int("ZENDNN_PRIMITIVE_LOG_ENABLE") == 3) {
+        stream->wait();
+        double start_ms = get_msec();
+        amdProfileResume();
+        status = stream->enqueue_primitive(primitive_iface, ctx);
+        stream->wait();
+        amdProfilePause();
+        double duration_ms = get_msec() - start_ms;
+        std::string stamp;
+        if (get_verbose_timestamp()) {
+            stamp = "," + std::to_string(start_ms);
+        }
+
+        zendnnInfo(ZENDNN_PROFLOG, "zendnn_primitive_execute,", stamp.c_str(),
+                   primitive_iface->pd()->info(), ",", duration_ms, ",ms");
+    }
+    else {
         status = stream->enqueue_primitive(primitive_iface, ctx);
     }
 
 #if defined(ZENDNN_ENABLE_ITT_TASKS)
-    if (enable_itt) itt::primitive_task_end();
+    if (enable_itt) {
+        itt::primitive_task_end();
+    }
 #endif
 
     stream->after_exec_hook();
 
-    if (msan_enabled) unpoison_outputs(ctx.args());
+    if (msan_enabled) {
+        unpoison_outputs(ctx.args());
+    }
 
     return status;
 }
@@ -170,34 +240,35 @@ status_t primitive_execute(
 
 // API
 status_t zendnn_primitive_desc_destroy(
-        primitive_desc_iface_t *primitive_desc_iface) {
+    primitive_desc_iface_t *primitive_desc_iface) {
     delete primitive_desc_iface;
     return success;
 }
 
 status_t zendnn_primitive_create(primitive_iface_t **primitive_iface,
-        const primitive_desc_iface_t *primitive_desc_iface) {
-    if (utils::any_null(primitive_iface, primitive_desc_iface))
+                                 const primitive_desc_iface_t *primitive_desc_iface) {
+    if (utils::any_null(primitive_iface, primitive_desc_iface)) {
         return invalid_arguments;
+    }
 
 #ifdef ZENDNN_ENABLE_STACK_CHECKER
     stack_checker::stack_checker_t sc("zendnn_primitive_create");
     bool is_wino = std::string(primitive_desc_iface->info()).find("wino")
-            != std::string::npos;
+                   != std::string::npos;
 
     if (!is_wino) {
         const cache_blob_t dummy;
         return sc.check(zendnn::impl::primitive_create, primitive_iface,
-                primitive_desc_iface, std::ref(dummy));
+                        primitive_desc_iface, std::ref(dummy));
     }
 #endif
     return zendnn::impl::primitive_create(primitive_iface, primitive_desc_iface);
 }
 
 status_t zendnn_primitive_create_from_cache_blob(
-        primitive_iface_t **primitive_iface,
-        const primitive_desc_iface_t *primitive_desc_iface, size_t size,
-        const uint8_t *cache_blob) {
+    primitive_iface_t **primitive_iface,
+    const primitive_desc_iface_t *primitive_desc_iface, size_t size,
+    const uint8_t *cache_blob) {
     if (utils::any_null(primitive_iface, primitive_desc_iface, cache_blob)
             || size == 0) {
         return invalid_arguments;
@@ -206,50 +277,56 @@ status_t zendnn_primitive_create_from_cache_blob(
     const auto runtime_kind = primitive_desc_iface->engine()->runtime_kind();
     if (ekind != engine_kind::gpu
             || (ekind == engine_kind::gpu
-                    && runtime_kind != runtime_kind::ocl)) {
+                && runtime_kind != runtime_kind::ocl)) {
         return status::unimplemented;
     }
 
     cache_blob_t cb(const_cast<uint8_t *>(cache_blob), size);
     return zendnn::impl::primitive_create(
-            primitive_iface, primitive_desc_iface, cb);
+               primitive_iface, primitive_desc_iface, cb);
 }
 
 status_t zendnn_primitive_execute(const primitive_iface_t *primitive_iface,
-        stream_t *stream, int nargs, const zendnn_exec_arg_t *c_args) {
+                                  stream_t *stream, int nargs, const zendnn_exec_arg_t *c_args) {
     bool ok = true && !utils::any_null(primitive_iface, stream)
-            && primitive_iface->engine() == stream->engine()
-            && IMPLICATION(nargs > 0, c_args != nullptr);
-    if (!ok) return invalid_arguments;
+              && primitive_iface->engine() == stream->engine()
+              && IMPLICATION(nargs > 0, c_args != nullptr);
+    if (!ok) {
+        return invalid_arguments;
+    }
 
     exec_args_t args;
     status_t status = cvt_primitive_args(
-            primitive_iface->pd()->impl().get(), nargs, c_args, args);
-    if (status != status::success) return status;
+                          primitive_iface->pd()->impl().get(), nargs, c_args, args);
+    if (status != status::success) {
+        return status;
+    }
 
     exec_ctx_t ctx(stream, std::move(args));
 #ifdef ZENDNN_ENABLE_STACK_CHECKER
     stack_checker::stack_checker_t sc("zendnn_primitive_execute");
     const auto *pd_iface = primitive_iface->pd();
     bool is_wino
-            = std::string(pd_iface->info()).find("wino") != std::string::npos;
+        = std::string(pd_iface->info()).find("wino") != std::string::npos;
     if (!is_wino) {
         return sc.check(
-                zendnn::impl::primitive_execute, primitive_iface, std::ref(ctx));
+                   zendnn::impl::primitive_execute, primitive_iface, std::ref(ctx));
     }
 #endif
     return zendnn::impl::primitive_execute(primitive_iface, ctx);
 }
 
 status_t zendnn_primitive_get_primitive_desc(
-        const primitive_iface_t *primitive_iface,
-        const primitive_desc_iface_t **primitive_desc_iface) {
-    if (utils::any_null(primitive_iface, primitive_desc_iface))
+    const primitive_iface_t *primitive_iface,
+    const primitive_desc_iface_t **primitive_desc_iface) {
+    if (utils::any_null(primitive_iface, primitive_desc_iface)) {
         return invalid_arguments;
+    }
     return safe_ptr_assign(*primitive_desc_iface, primitive_iface->pd());
 }
 
-status_t zendnn_primitive_get_cache_blob(const primitive_iface_t *primitive_iface,
+status_t zendnn_primitive_get_cache_blob(const primitive_iface_t
+        *primitive_iface,
         size_t *size, uint8_t *cache_blob) {
     if (utils::any_null(primitive_iface, size)) {
         return status::invalid_arguments;
@@ -259,7 +336,7 @@ status_t zendnn_primitive_get_cache_blob(const primitive_iface_t *primitive_ifac
     const auto runtime_kind = primitive_iface->engine()->runtime_kind();
     if (ekind != engine_kind::gpu
             || (ekind == engine_kind::gpu
-                    && runtime_kind != runtime_kind::ocl)) {
+                && runtime_kind != runtime_kind::ocl)) {
         return status::unimplemented;
     }
 
@@ -275,21 +352,24 @@ status_t zendnn_primitive_get_cache_blob(const primitive_iface_t *primitive_ifac
 }
 
 status_t zendnn_primitive_destroy(primitive_iface_t *primitive_iface) {
-    if (primitive_iface != nullptr) primitive_iface->release();
+    if (primitive_iface != nullptr) {
+        primitive_iface->release();
+    }
     return success;
 }
 
 // primitive_iface_t implementation
 zendnn_primitive::zendnn_primitive(
-        const std::shared_ptr<primitive_t> &primitive, engine_t *engine)
+    const std::shared_ptr<primitive_t> &primitive, engine_t *engine)
     : counter_(1)
     , primitive_(primitive)
     , pd_(utils::make_unique<primitive_desc_iface_t>(
               primitive_->pd(), engine)) {}
 
 // reorder specialization
-zendnn_primitive::zendnn_primitive(const std::shared_ptr<primitive_t> &primitive,
-        engine_t *engine, engine_t *src_engine, engine_t *dst_engine)
+zendnn_primitive::zendnn_primitive(const std::shared_ptr<primitive_t>
+                                   &primitive,
+                                   engine_t *engine, engine_t *src_engine, engine_t *dst_engine)
     : counter_(1)
     , primitive_(primitive)
     , pd_(utils::make_unique<reorder_primitive_desc_iface_t>(
@@ -299,25 +379,27 @@ zendnn_primitive::~zendnn_primitive() {
     if (scratchpad_debug::is_protect_scratchpad() && scratchpad_ != nullptr
             && scratchpad_->get_memory_storage() != nullptr) {
         const memory_tracking::registry_t &registry
-                = primitive_->pd()->scratchpad_registry();
+            = primitive_->pd()->scratchpad_registry();
         scratchpad_debug::unprotect_scratchpad_buffer(
-                scratchpad_->get_memory_storage(), registry);
+            scratchpad_->get_memory_storage(), registry);
     }
 }
 
 status_t zendnn_primitive::init() {
     const size_t scratchpad_size
-            = primitive_->pd()->scratchpad_size(scratchpad_mode::library);
+        = primitive_->pd()->scratchpad_size(scratchpad_mode::library);
 
     if (scratchpad_size) {
         const memory_tracking::registry_t &registry
-                = primitive_->pd()->scratchpad_registry();
+            = primitive_->pd()->scratchpad_registry();
         bool use_global_scratchpad = scratchpad_debug::is_protect_scratchpad()
-                ? false
-                : primitive_->use_global_scratchpad();
+                                     ? false
+                                     : primitive_->use_global_scratchpad();
         auto *scratchpad_ptr = create_scratchpad(
-                pd_->engine(), scratchpad_size, use_global_scratchpad);
-        if (scratchpad_ptr == nullptr) return out_of_memory;
+                                   pd_->engine(), scratchpad_size, use_global_scratchpad);
+        if (scratchpad_ptr == nullptr) {
+            return out_of_memory;
+        }
         if (scratchpad_ptr->get_memory_storage() == nullptr) {
             delete scratchpad_ptr;
             return out_of_memory;
@@ -325,10 +407,12 @@ status_t zendnn_primitive::init() {
 
         if (scratchpad_debug::is_protect_scratchpad()) {
             scratchpad_debug::protect_scratchpad_buffer(
-                    scratchpad_ptr->get_memory_storage(), registry);
+                scratchpad_ptr->get_memory_storage(), registry);
         }
         scratchpad_.reset(scratchpad_ptr);
-        if (scratchpad_ptr->size() < scratchpad_size) return out_of_memory;
+        if (scratchpad_ptr->size() < scratchpad_size) {
+            return out_of_memory;
+        }
     }
     return primitive_->create_resource(pd()->engine(), resource_mapper_);
 }
@@ -346,13 +430,14 @@ status_t zendnn_primitive::execute(exec_ctx_t &ctx) const {
     if (primitive_->pd()->attr()->scratchpad_mode_ == scratchpad_mode::user) {
         memory_t *scratchpad_memory = ctx.output(ZENDNN_ARG_SCRATCHPAD);
         mem_storage = scratchpad_memory ? scratchpad_memory->memory_storage()
-                                        : nullptr;
-    } else if (scratchpad_) {
+                      : nullptr;
+    }
+    else if (scratchpad_) {
         mem_storage = scratchpad_->get_memory_storage();
     }
 
     auto scratchpad_grantor
-            = primitive_->pd()->scratchpad_registry().grantor(mem_storage, ctx);
+        = primitive_->pd()->scratchpad_registry().grantor(mem_storage, ctx);
     ctx.set_scratchpad_grantor(&scratchpad_grantor);
     ctx.set_resource_mapper(&resource_mapper_);
 
