ef87e41fc9c89b8deafc231b70a0a2fb1aeb9b99
Avinash Pandey <avinash-chandra.pandey@amd.com>
2023-09-11 03:11:42 +0000

    [ZENDNN ALGO] Added AVX512 support for EB kernel
    1. Added AVX512 support for EB kernel
    
    Signed-off-by: Avinash Pandey <avinash-chandra.pandey@amd.com>
    Change-Id: Ib70ea1cd3dff288f3a456f61421fdfc6820ba9ea
    Reviewed-on: https://gerrit-git.amd.com/c/amd/ec/ZenDNN/+/922550
    Tested-by: Llvm Cpu <llvm.cpu@amd.com>
    Reviewed-by: Sudarshan Shinde <Sudarshan.Shinde@amd.com>

---
+++ src/cpu/avx512_embedding_bag.hpp
@@ -0,0 +1,106 @@
+/*******************************************************************************
+* Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*******************************************************************************/
+
+#ifndef CPU_AVX512_EMBEDDING_BAG_HPP
+#define CPU_AVX512_EMBEDDING_BAG_HPP
+
+#include <iostream>
+#include <assert.h>
+#include <cstdint>
+
+#include "common/c_types_map.hpp"
+#include "common/primitive.hpp"
+#include "common/type_helpers.hpp"
+#include "common/utils.hpp"
+#include "zendnn_helper.hpp"
+
+#include "cpu/platform.hpp"
+#include "cpu/primitive_attr_postops.hpp"
+#include "cpu/x64/cpu_isa_traits.hpp"
+
+#include "cpu/cpu_embedding_bag_pd.hpp"
+#include "cpu/avx2_embedding_bag.hpp"
+
+#define  ALIGNED_AVX512_UNSAFE(addr)   ((uint64_t)(addr) & 0x3F)
+
+namespace zendnn {
+namespace impl {
+namespace cpu {
+
+template <impl::data_type_t data_type>
+struct avx512_embedding_bag_t : public primitive_t {
+    struct pd_t : public cpu_embedding_bag_pd_t {
+        using cpu_embedding_bag_pd_t::cpu_embedding_bag_pd_t;
+        using input_type   = typename prec_traits<data_type>::type;
+        using indices_type = int32_t;
+        using offsets_type = int32_t;
+
+        DECLARE_COMMON_PD_T("avx512:any", avx512_embedding_bag_t);
+
+        status_t init(engine_t *engine) {
+            if (! platform::has_data_type_support(data_type) ||
+                    !x64::mayiuse(x64::avx512_core)) {
+                return status::unimplemented;
+            }
+
+            bool eb_avx2 = zendnn_getenv_int("ZENDNN_EBAVX2_ENABLE", 0);
+            if (eb_avx2) {
+                return status::unimplemented;
+            }
+
+            return status::success;
+        }
+    };
+    // constructor using pd_t
+    avx512_embedding_bag_t(const pd_t *apd) : primitive_t(apd) {}
+
+    // init() override from primitive_t
+    status_t init(engine_t *engine) override {
+        return status::success;
+    }
+
+    using input_type   = typename prec_traits<data_type>::type;
+    using indices_type = int32_t;
+    using offsets_type = int32_t;
+    using dst_type     = input_type;
+
+    // exec() override from primitive_t
+    status_t execute(const exec_ctx_t &ctx) const override;
+
+  private:
+    const pd_t *pd() const {
+        return (const pd_t *)primitive_t::pd().get();
+    }
+
+    status_t pre_process(const exec_ctx_t &ctx,
+                         emb_params_t &params) const;
+    status_t avx512_sum(const emb_params_t &params) const;
+    status_t avx512_sum_wt(const emb_params_t &params) const;
+
+    status_t avx512_mean(const emb_params_t &params) const;
+    status_t avx512_max(const emb_params_t &params) const;
+    void ebvec_prefetch(float const *input, indices_type *indices,
+                        const int32_t width, const int32_t size) const;
+
+
+};
+
+} // namespace cpu
+} // namespace impl
+} // namespace zendnn
+
+#endif
--- src/cpu/cpu_embedding_bag_list.cpp
+++ src/cpu/cpu_embedding_bag_list.cpp
@@ -18,6 +18,7 @@
 #include "cpu/cpu_engine.hpp"
 #include "cpu/ref_embedding_bag.hpp"
 #include "cpu/avx2_embedding_bag.hpp"
+#include "cpu/avx512_embedding_bag.hpp"
 
 namespace zendnn {
 namespace impl {
@@ -30,13 +31,16 @@ using namespace zendnn::impl::prop_kind;
 /* add new primitive */
 // clang-format off
 const std::map<pk_impl_key_t, std::vector<impl_list_item_t>> &impl_list_map() {
-    static const std::map<pk_impl_key_t, std::vector<impl_list_item_t>> the_map = REG_EMBEDDING_BAG_P({
-        {{forward}, {
-            CPU_INSTANCE(avx2_embedding_bag_t<f32>)
-            CPU_INSTANCE(ref_embedding_bag_t<f32>)
-            /* eol */
-            nullptr,
-        }},
+    static const std::map<pk_impl_key_t, std::vector<impl_list_item_t>> the_map =
+    REG_EMBEDDING_BAG_P({
+        {   {forward}, {
+                CPU_INSTANCE(avx512_embedding_bag_t<f32>)
+                CPU_INSTANCE(avx2_embedding_bag_t<f32>)
+                CPU_INSTANCE(ref_embedding_bag_t<f32>)
+                /* eol */
+                nullptr,
+            }
+        },
     });
     return the_map;
 }
@@ -45,18 +49,18 @@ const std::map<pk_impl_key_t, std::vector<impl_list_item_t>> &impl_list_map() {
 
 
 const impl_list_item_t *get_embedding_bag_impl_list(
-        const embedding_bag_desc_t *desc) {
+    const embedding_bag_desc_t *desc) {
     static const impl_list_item_t empty_list[] = {nullptr};
 
     const bool is_fwd = utils::one_of(
-            desc->prop_kind, forward_training, forward_inference);
+                            desc->prop_kind, forward_training, forward_inference);
     prop_kind_t prop_kind = is_fwd ? forward : backward;
 
     pk_impl_key_t key {prop_kind};
 
     const auto impl_list_it = impl_list_map().find(key);
     return impl_list_it != impl_list_map().cend() ? impl_list_it->second.data()
-                                                  : empty_list;
+           : empty_list;
 }
 
 } // namespace cpu
