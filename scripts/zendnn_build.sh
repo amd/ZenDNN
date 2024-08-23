ed8e4bf08a913760b69d1f5db64bc339bb467500
Kiriti Pendyala <kiriti.pendyala@amd.com>
2024-08-23 19:11:18 +0530

    [ZENDNN CORE] Updated zendnn build script
    Updated modification year and shebang line
    
    Change-Id: Idbfaceaa319c2d338eb363563e642cfb1fe3b627
    Signed-off-by: Kiriti Pendyala <kiriti.pendyala@amd.com>
    Reviewed-on: http://gerrit-git.amd.com/c/amd/ec/ZenDNN/+/1111191
    Tested-by: Llvm Cpu <llvm.cpu@amd.com>
    Reviewed-by: Ratan Prasad <Ratan.Prasad2@amd.com>
    Reviewed-by: Ayush SinghBaiswar <Ayush.SinghBaiswar@amd.com>
    Reviewed-by: Avinash-Chandra Pandey <Avinash-Chandra.Pandey@amd.com>

--- scripts/zendnn_build.sh
+++ scripts/zendnn_build.sh
@@ -1,5 +1,6 @@
-﻿#*******************************************************************************
-# Copyright (c) 2019-2022 Advanced Micro Devices, Inc. All rights reserved.
+﻿#!/bin/bash
+#*******************************************************************************
+# Copyright (c) 2019-2024 Advanced Micro Devices, Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,7 +16,6 @@
 #
 #*******************************************************************************
 
-#!/bin/bash
 
 if [[ "$1" == "aocc" ]]; then
     chmod u+x ./scripts/zendnn_aocc_env_setup.sh
