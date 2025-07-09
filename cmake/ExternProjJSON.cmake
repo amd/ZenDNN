73af4e6aa566526113620a9d6920982664ff5dd5
Chinmay Kulkarni <chkulkar@amd.com>
2025-07-09 01:11:17 -0600

    [ZENDNNL BUILD] Build rectification to support json for zentorch
     - ZENAI-000
    
    Signed-off-by: Chinmay Kulkarni <chkulkar@amd.com>
    Change-Id: I186df34618d6635f8a8932747ae9e0e810b41840
    Reviewed-on: http://gerrit-git.amd.com/c/amd/ec/ZenDNN/+/1257939
    Tested-by: Llvm Cpu <llvm.cpu@amd.com>
    Reviewed-by: Sudarshan Shinde <Sudarshan.Shinde@amd.com>

--- cmake/ExternProjJSON.cmake
+++ cmake/ExternProjJSON.cmake
@@ -41,6 +41,35 @@ if(ZENDNNL_DEPENDS_JSON)
     ADDITIONAL_CLEAN_FILES "${JSON_CLEAN_FILES}")
 
   list(APPEND ZENDNNL_DEPS "zendnnl-deps-json")
+
+  # !!!
+  # HACK to make zendnnl a sub-project using add_directory() !
+  # ZenDNNL packages the exported information of this dependency in its own
+  # package config file. However to use this information, ZenDNNL package
+  # need to be installed.
+  # when ZenDNNL is included as a sub-project using add_subdirectory it
+  # can not be installed before the super-project of which it is a sub-project
+  # is built. Thus super-project can not use its package information. This
+  # makes this hack of manually interfacing the libraries this dependency has
+  # built necessary.
+  # Since we do not know what kind of information and targets this package exports
+  # this kind of manual interface could be error-prone.
+  #
+  # UNCOMMENT the code below for manual interface.
+
+  set(ZENDNNL_JSON_INC_DIR "${CMAKE_INSTALL_PREFIX}/deps/json/include")
+
+  file(MAKE_DIRECTORY ${ZENDNNL_JSON_INC_DIR})
+  add_library(zendnnl_json_deps INTERFACE IMPORTED GLOBAL)
+  add_dependencies(zendnnl_json_deps zendnnl-deps-json)
+  set_target_properties(zendnnl_json_deps PROPERTIES
+    INTERFACE_COMPILE_DEFINITIONS "\$<\$<NOT:\$<BOOL:ON>>:JSON_USE_GLOBAL_UDLS=0>;\$<\$<NOT:\$<BOOL:ON>>:JSON_USE_IMPLICIT_CONVERSIONS=0>;\$<\$<BOOL:OFF>:JSON_DISABLE_ENUM_SERIALIZATION=1>;\$<\$<BOOL:OFF>:JSON_DIAGNOSTICS=1>;\$<\$<BOOL:OFF>:JSON_DIAGNOSTIC_POSITIONS=1>;\$<\$<BOOL:OFF>:JSON_USE_LEGACY_DISCARDED_VALUE_COMPARISON=1>"
+    INTERFACE_COMPILE_FEATURES "cxx_std_11"
+    INTERFACE_INCLUDE_DIRECTORIES "${ZENDNNL_JSON_INC_DIR};${ZENDNNL_JSON_INC_DIR}")
+
+  add_library(nlohmann_json::nlohmann_json ALIAS zendnnl_json_deps)
+  list(APPEND ZENDNNL_LINK_LIBS "nlohmann_json::nlohmann_json")
+
 else()
   message(DEBUG "skipping building json...")
 endif()
